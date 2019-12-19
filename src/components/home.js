faef1995ecc22b99e7b43d321ae68fe83f5d3ef0
syang1297 <syang01@umail.ucsb.edu>
2019-12-19 14:44:57 -0800

    added dropdown in navbar. WIP: need to add 3 stripes image

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
+
+const profPic = require('../profile.png');
+
+
 export default class Home extends Component {
   render() {
     return (
       <div>
-          Home test
+          <img src={profPic} width="200" height="200" alt="Picture of Shu"/>
       </div>
     )
   }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,27 @@
 import React from "react";
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
 
 import "../css/navbar.css"
+
+const dropDownIcon = require('../dropdown.png');
+
 
 export default class NavBar extends React.Component {
   render() {
     return (
       <div>
-        <Navbar variant="light" expand="xl" fixed="top" className="navbar">
+        <Navbar collapseOnSelect variant="light" expand="lg" fixed="top" className="navbar">
           <Navbar.Brand href="/">
-            <Navbar.Text className="navbarTitle">Home</Navbar.Text>
+            <Navbar.Text className="navbarTitle">Shu Yang</Navbar.Text>
           </Navbar.Brand>
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-            <Nav>
-              <Nav.Link href = "/about" className = "link">About</Nav.Link>
-              <Nav.Link href="/resume" className="link">Resume</Nav.Link>
-              <Nav.Link href="/projects" className="link">Projects</Nav.Link>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
+          <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+            <Nav className="mr-auto">
+                <NavDropdown src={dropDownIcon}  width="200" height = "200" id="collasible-nav-dropdown" className="">
+                    <NavDropdown.Item href="/about">About</NavDropdown.Item>
+                    <NavDropdown.Item href="/resume">Resume</NavDropdown.Item>
+                    <NavDropdown.Item href="/projects">Projects</NavDropdown.Item>
+                </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
